4f7e23f7e6528b422fe63e56cb7f9d970c567191
egordylan <egordylan@gmail.com>
2022-07-15 17:49:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configToFind = '';
+  let coeff = 1;
+
+  while (coeff > 0 && str.length > 0) {
+    coeff = 0;
+    bracketsConfig.map(elem => {
+      configToFind = elem.join('');
+      if (str.includes(configToFind)) {
+        str = str.replace(configToFind, '');
+        coeff++;
+      }
+    }
+    );
+  }
+  return (str.length === 0 ? true : false);
 }
